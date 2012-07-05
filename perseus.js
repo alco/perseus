01c6e41f7bac2b096c7c8f42444adda653e216b0
Alexei Sholik <alcosholik@gmail.com>
2012-07-06 00:36:06 +0300

    Initial commit

---
+++ perseus.js
@@ -0,0 +1,130 @@
+/*
+ * Dependencies: jQuery, Underscore.js
+ */
+var Perseus = new function() {
+    this._refs = {};
+
+    this.reset = function() {
+        for (var key in this._refs) {
+            localStorage.setItem(key, undefined);
+        }
+        this._refs = {};
+    };
+
+    this.persistentObject = function(key, obj) {
+        var db_object = localStorage.getItem(key);
+        if (db_object !== null && db_object != "__Perseus_null__") {
+            db_object = JSON.parse(db_object);
+            // clone obj
+            var instance = $.extend(true, {}, obj);
+            for (var prop in db_object) {
+                instance[prop] = db_object[prop];
+            }
+            console.log("Loaded from localStorage");
+            this._refs[key] = instance;
+            return instance;
+        } else {
+            localStorage.setItem(key, JSON.stringify(obj));
+            this._refs[key] = obj;
+            return obj;
+        }
+    };
+
+    function deserialize(obj) {
+        var result;
+        switch (obj.type) {
+        case "array":
+            result = _.map(obj.val, function(val, index) {
+                return deserialize(val);
+            });
+            break;
+
+        case "function":
+            eval(obj.val);
+            result = f;
+            break;
+
+        case "number":
+            result = parseFloat(obj.val);
+            break;
+
+        case "string":
+            result = obj.val;
+            break;
+
+        case "object":
+            result = {};
+            for (var prop in obj.val) {
+                if (!obj.val.hasOwnProperty(prop))
+                    continue;
+
+                var val = obj.val[prop];
+                result[prop] = deserialize(val);
+            }
+            break;
+
+        default:
+            if (obj.type === undefined) {
+                result = obj;
+            } else {
+                console.log("Unhandle case in deserialize: " + obj.type);
+            }
+        }
+        return result;
+    };
+
+    this.lookupObject = function(key) {
+        var db_object = localStorage.getItem(key);
+        if (db_object === null)
+            return null;
+
+        return deserialize(JSON.parse(db_object));
+    };
+
+    function serialize(obj) {
+        var db_object;
+        if (_.isArray(obj)) {
+            db_object = {
+                type: "array",
+                val: _.map(obj, function(val, index) {
+                    return serialize(val);
+                }),
+            };
+        } else if (_.isObject(obj) && !_.isEmpty(obj)) {
+            db_object = {};
+            for (var prop in obj) {
+                if (!obj.hasOwnProperty(prop))
+                    continue;
+
+                var result;
+                var val = obj[prop];
+                db_object[prop] = serialize(val);
+            }
+        } else if (_.isFunction(obj)) {
+            db_object = { type: "function", val: obj.toString() };
+        } else if (_.isNumber(obj)) {
+            db_object = { type: "number", val: obj };
+        } else if (_.isString(obj)) {
+            db_object = { type: "string", val: obj };
+        } else {
+            db_object = { type: "object", val: obj };
+        }
+        return db_object;
+    };
+
+    this.storeObject = function(key, obj) {
+        var db_object = serialize(obj);
+        localStorage.setItem(key, JSON.stringify(db_object));
+    };
+
+    this.removeObject = function(key) {
+        localStorage.setItem(key, "__Perseus_null__");
+        this._refs[key] = undefined;
+    };
+
+    this.store = function() {
+        for (var key in this._refs) {
+            localStorage.setItem(key, JSON.stringify(this._refs[key]));
+        }
+    };
+};
---
+++ test.js
@@ -0,0 +1,133 @@
+Object.prototype.equals = function(x)
+{
+  var p;
+  for(p in this) {
+      if(typeof(x[p])=='undefined') {return false;}
+  }
+
+  for(p in this) {
+      if (this[p]) {
+          switch(typeof(this[p])) {
+              case 'object':
+                  if (!this[p].equals(x[p])) { return false; } break;
+              case 'function':
+                  if (typeof(x[p])=='undefined' ||
+                      (p != 'equals' && this[p].toString() != x[p].toString()))
+                      return false;
+                  break;
+              default:
+                  if (this[p] != x[p]) { return false; }
+          }
+      } else {
+          if (x[p])
+              return false;
+      }
+  }
+
+  for(p in x) {
+      if(typeof(this[p])=='undefined') {return false;}
+  }
+
+  return true;
+}
+
+module("Perseus")
+
+test("empty object", function() {
+    var obj = {};
+    Perseus.storeObject('empty obj', obj);
+    ok(obj.equals(Perseus.lookupObject('empty obj')));
+
+});
+
+test("empty array", function() {
+    var obj = [];
+    Perseus.storeObject('empty array', obj);
+    ok(obj.equals(Perseus.lookupObject('empty array')));
+});
+
+test("number array", function() {
+    var obj = [1, 2, 0, -.4, 1.3e-8];
+    Perseus.storeObject('number array', obj);
+    ok(obj.equals(Perseus.lookupObject('number array')));
+});
+
+test("string array", function() {
+    var obj = ["", "a", 'hello', '-1'];
+    Perseus.storeObject('string array', obj);
+    ok(obj.equals(Perseus.lookupObject('string array')));
+});
+
+test("interleaved array", function() {
+    var obj = ["", 0, 2, "bye", {}];
+    Perseus.storeObject('interleaved array', obj);
+    ok(obj.equals(Perseus.lookupObject('interleaved array')));
+});
+
+test("nested array", function() {
+    var obj = ["", ["a", []], ['hello', ['-1']]];
+    Perseus.storeObject('nested array', obj);
+    ok(obj.equals(Perseus.lookupObject('nested array')));
+});
+
+/*
+test("set:add", function() {
+  var set = new Set()
+  for (var i = 0; i < 100; ++i) {
+    set.add(i)
+  }
+  equal(set.length, 100, "Set must have exactly 100 elements")
+  ok(set.contains(99), "99 must be in the set")
+  ok(!set.contains(100), "100 must not be in the set")
+
+  for (var i = 50; i < 101; ++i) {
+    set.add(i)
+  }
+  equal(set.length, 101, "Now the set has 101 elements")
+  ok(set.contains(100), "100 is now in the set")
+  ok(!set.contains(101), "101 is not yet in the set")
+})
+
+test("set:remove", function() {
+  var set = new Set([1, 2, 3, 3, 3, 4, 5, 6, 7, 8, 8, 9, 10, 10])
+  equal(set.length, 10, "Set length must be 10")
+
+  ok(set.contains(3), "3 is in the set")
+  set.remove(3)
+  equal(set.length, 9, "One element less")
+  ok(!set.contains(3), "3 is not in the set")
+
+  set.remove(3)
+  equal(set.length, 9, "The same length")
+  ok(!set.contains(3), "3 is not here already")
+})
+
+test("set:join", function() {
+  var set = new Set([1,2,3,4,5])
+  var second_set = new Set([3,4,5,6,7,8,9])
+
+  var joined = set.splice(second_set)
+  for (var i = 1; i < 10; ++i)
+    ok(joined.contains(i), "Joined must contain " + i)
+  equal(joined.length, 9, "It must have exactly 9 elements")
+  same(joined.items(), ["1","2","3","4","5","6","7","8","9"])
+  equal(set.length, 5, "First set is left intact")
+  equal(second_set.length, 7, "Second set as well")
+})
+
+
+test("set:pick", function() {
+  var set = new Set([1,2,3,4,5,6])
+
+  equal(set.length, 6)
+  var failCounter = 10
+  var iterCounter = 0
+  while (!set.isEmpty()) {
+    ++iterCounter
+    console.log(set.pick())
+    if (--failCounter == 0) break
+  }
+  equal(iterCounter, 6, "Exactly 6 iterations expected")
+  equal(set.length, 0)
+})
+*/
